6ee21246ba938b048124b3db5f221e6646aab0ea
Ayush Dhatwalia <ayushdhatwalia2005@gmail.com>
2025-12-10 18:20:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ tokenizer.fit_on_texts(tweet['text'].astype(str))
 word_index = tokenizer.word_index
 
 # Load the pre-trained model with ReLU activation
-model = load_model('TweeterSentimentAnalysis.h5')
+model = load_model("TweeterSentimentAnalysis.h5", compile=False)
 
 # Function to preprocess user input
 def preprocess_text(text):
@@ -52,5 +52,6 @@ if st.button('Classify'):
     st.write(f'Prediction Score: {1-prediction[0][0]}')
 else:
     st.write('Please enter a Tweet.')
+
 
 
